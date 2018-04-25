652536f352bd7dddbcdc62ae208a506e226f923f
Joseph Chee Chang <josephcc@cmu.edu>
2018-04-25 18:18:10 -0400

    Wikipedia Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 
-import styled from "styled-components"
+import styled from 'styled-components'
 
 import CssBaseline from 'material-ui/CssBaseline'
 import Grid from 'material-ui/Grid'
-import Card, { CardContent } from 'material-ui/Card'
+import Card, { CardContent, CardActions, CardMedia } from 'material-ui/Card'
 import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
+import TextField from 'material-ui/TextField'
 
+import Search from '@material-ui/icons/Search'
 import blue from 'material-ui/colors/blue'
 import green from 'material-ui/colors/green'
+import grey from 'material-ui/colors/grey'
 
-
+import WikipediaCard from './wikipedia_card.jsx'
 import data from './barcelona.json'
 
 const Title = styled(({className, children, ...props}) => (
@@ -50,27 +54,52 @@ class App extends Component {
   static defaultProps = {
     ...data
   }
+
+	handleExploreEntity(tag) {
+    console.log(tag)
+	}
+
   render() {
     return (<React.Fragment><CssBaseline />
+			<Grid container spacing={8} alignItems='flex-end' style={{padding: '12px 32px 0px 24px'}}>
+				<Grid item>
+					<Search/>
+				</Grid>
+				<Grid item xs={4}>
+					<TextField id='input-with-icon-grid' label='Search the Web' value='Day trips from Barcelona' style={{width: '100%'}} disabled/>
+				</Grid>
+			</Grid>
 
-      <div style={{padding: '32px', width: '600px'}}>
-        <Grid container spacing={24}>
-          <Grid item xs={12}>
-            { this.props.bookmarks.map((bookmark) => {
-              return (
-								<Card style={{padding: '4px', margin: '12px'}}>
-									<CardContent>
-                    <Title url={bookmark.url}>{bookmark.title}</Title>
-                    <Url url={bookmark.url}/>
-										<Typography component='p' style={{marginTop: '12px'}}>
-                      { bookmark.desc }
-										</Typography>
-									</CardContent>
-								</Card>
-              )
-            }) }
+      <div style={{display: 'flex', overflowX: 'auto', background: grey[800], padding: '32px', margin: '32px 0px'}}>
+				{ this.props.clusters[0].tags.map((tag, idx) => {
+          return (
+            <WikipediaCard entityName={tag} onExploreEntity={this.handleExploreEntity.bind(this, tag)} key={`cluster.0.tag.${idx}`}/>
+          )
+				})}
+      </div>
+
+      <div style={{display: 'flex'}}>
+
+        <div style={{padding: '32px', maxWidth: '600px'}}>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              { this.props.bookmarks.map((bookmark, idx) => {
+                return (
+                  <Card style={{padding: '4px', margin: '12px'}} key={`bookmark.${idx}`}>
+                    <CardContent>
+                      <Title url={bookmark.url}>{bookmark.title}</Title>
+                      <Url url={bookmark.url}/>
+                      <Typography component='p' style={{marginTop: '12px'}}>
+                        { bookmark.desc }
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                )
+              }) }
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
+
       </div>
 
     </React.Fragment>);
---
+++ src/wikipedia_card.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+import styled from 'styled-components'
+
+import Card, { CardContent, CardActions, CardMedia } from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
+
+import blue from 'material-ui/colors/blue'
+import green from 'material-ui/colors/green'
+import grey from 'material-ui/colors/grey'
+
+import { get } from 'lodash'
+
+
+class WikipediaCard extends Component {
+  static defaultProps = {
+  }
+
+  constructor(props) {
+    super(props)
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+    this.state = {
+      normalizedEntityName: props.entityName,
+      image: 'http://thecatapi.com/api/images/get?format=src&type=gif',
+      desc: 'loading...',
+      loaded: false
+    }
+  }
+
+  componentDidMount() {
+    this.componentWillReceiveProps(this.props)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let entity = encodeURIComponent(nextProps.entityName)
+
+    let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${entity}&limit=1&format=json&redirects=resolve&origin=*`
+    fetch(url).then( (response) => {
+      return response.json()
+    }).then((results) => {
+      this.setState({loaded: true, desc: results[2][0]})
+    }).catch((reason) => {
+      console.log(reason)
+      this.setState({loaded: true, desc: reason})
+      throw reason
+    });
+
+    let image = `https://en.wikipedia.org/w/api.php?action=query&titles=${entity}&prop=pageimages&format=json&pithumbsize=200&origin=*`
+    fetch(image).then( (response) => {
+      return response.json()
+    }).then((results) => {
+      let image = get( Object.values(results.query.pages), '[0].thumbnail.source', undefined)
+      if (image !== undefined) {
+        this.setState({image: image})
+      }
+    }).catch((reason) => {
+      console.log(reason)
+      throw reason
+    });
+  }
+
+  render() {
+    return (
+      <Card style={{margin: '12px', minWidth: '300px', height: '350px'}}>
+        <CardMedia
+          style={{height: '125px', backgroundRepeat: 'no-repeat', backgroundPosition: 'center top'}}
+          image={this.state.image}
+          title={this.props.entityName}
+        />
+        <CardContent>
+          <Typography gutterBottom variant='headline' component='h2'>
+            {this.props.entityName}
+          </Typography>
+          <Typography component='p' style={{height: '80px', overflow: 'hidden', textOverflow: 'ellipsis'}}>
+            { this.state.desc.replace(/\s*\([^)]*\)+\s*/, ' ') }
+          </Typography>
+        </CardContent>
+        { this.state.loaded && (
+          <CardActions>
+            <Button onClick={this.props.onExploreEntity} size='small' variant="raised" color="primary">
+              See mention
+            </Button>
+          </CardActions>
+        )}
+      </Card>
+    )
+  }
+}
+
+export default WikipediaCard;
